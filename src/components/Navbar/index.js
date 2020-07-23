22e6a92f1348a0fe6c4d45efbebc09694e2106f1
Enos Domingues <enosdomingues@gmail.com>
2020-07-23 00:17:31 -0300

    DropdownProvider

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,35 +2,38 @@ import React from 'react';
 
 import { Products, Developers, Company } from '../Content'
 import { Container, DropDownStyles } from './styles';
-import { DropdownOption } from '../Dropdown';
+import { DropdownOption, DropdownProvider } from '../Dropdown';
 
 
 function Navbar() {
   return (
-    <DropDownStyles>
-      <Container>
-        <ul>
-          <li>
-            <DropdownOption 
-                name="Produtos"
-                content={Products}
-            />
-          </li>
-          <li>
-            <DropdownOption 
-                name="Desenvolvedores"
-                content={Developers}
-            />
-          </li>
-          <li>
-            <DropdownOption 
-                name="Empresa"
-                content={Company}
-            />
-          </li>
-        </ul>
-      </Container>
-   </DropDownStyles>
+    <DropdownProvider>
+      <DropDownStyles>
+        <Container>
+          <ul>
+            <li>
+              <DropdownOption 
+                  name="Produtos"
+                  content={Products}
+              />
+            </li>
+            <li>
+              <DropdownOption 
+                  name="Desenvolvedores"
+                  content={Developers}
+              />
+            </li>
+            <li>
+              <DropdownOption 
+                  name="Empresa"
+                  content={Company}
+              />
+            </li>
+          </ul>
+        </Container>
+    </DropDownStyles>
+    </DropdownProvider>
+
   )
 }
 
